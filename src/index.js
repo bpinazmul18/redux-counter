8ea50e88abc3470c2ffe4bf38df436476a7b5011
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2021-02-22 11:42:44 +0600

    gitignore file add to .prettierrc, .idea, .package-lock.json file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { bindActionCreators, createStore } from 'redux';
+import { createStore } from 'redux';
 import { connect, Provider } from 'react-redux';
 
 import './styles.scss';
